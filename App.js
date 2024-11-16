c03d389975f038e198fba69898458a286446ea0b
Stefany Ramos A. <106556322+stefanyralvgh@users.noreply.github.com>
2024-11-16 15:31:24 -0500

    Add files via upload
    First try: header and footer

---
+++ App.js
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+import LittleLemonHeader from "./components/LittleLemonHeader";
+import LittleLemonFooter from "./components/LittleLemonFooter";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#495E57",
+      }}
+    >
+      <LittleLemonHeader />
+      <LittleLemonFooter />
+    </View>
+  );
+}
